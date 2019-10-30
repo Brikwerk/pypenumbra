770c08db60cedbab07773c880fc52e527032b856
Brikwerk <reece@brikwerk.com>
2019-10-30 01:02:39 -0700

    Added focal spot reconstruction and fixed return

--- pypenumbra/api.py
+++ pypenumbra/api.py
@@ -1,8 +1,11 @@
+import os
+
 from . import sinogram
 from skimage import io
 from skimage import img_as_ubyte, img_as_float64
-from skimage.color import rgb2gray
+from skimage.transform import iradon
 import numpy as np
+
 
 def reconstruct_from_image(image_path):
     # Attempting to load an image in grayscale
@@ -11,11 +14,18 @@ def reconstruct_from_image(image_path):
     float_image = img_as_float64(image)
     uint8_image = img_as_ubyte(image)
 
-    reconstruct(float_image, uint8_image)
+    return reconstruct(float_image, uint8_image)
 
 def reconstruct_from_ctdata(data_path):
     # TODO: Load raw ct image data
     print("test")
 
 def reconstruct(float_image, uint8_image):
+    # Getting sinogram
     sinogram_image = sinogram.construct_sinogram(float_image, uint8_image)
+
+    # Reconstructing the focal spot with filtered backprojection
+    theta = np.linspace(0., 360., max(sinogram_image.shape), endpoint=False)
+    focal_spot_image = iradon(sinogram_image, theta=theta, circle=True)
+
+    return focal_spot_image, sinogram_image

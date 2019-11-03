fbd8c408e7bfe3b3150b9e2646476a5fe81c7fef
Brikwerk <reece@brikwerk.com>
2019-11-03 14:45:04 -0800

    Added sinogram/focal spot output to results dir

--- demo.py
+++ demo.py
@@ -1,4 +1,10 @@
 import pypenumbra
-import cv2
+from skimage import io, img_as_ubyte
+from skimage.exposure import equalize_adapthist
+import os
 
-pypenumbra.reconstruct_from_image("./pypenumbra/test/data/penumbra_test.tif")
+focal_spot, sinogram = pypenumbra.reconstruct_from_image("./pypenumbra/test/data/penumbra_test_square.tif")
+if not os.path.isdir("./results"):
+    os.makedirs("./results")
+io.imsave("./results/focal_spot.png", img_as_ubyte(equalize_adapthist(focal_spot)))
+io.imsave("./results/sinogram.png", img_as_ubyte(equalize_adapthist(sinogram)))

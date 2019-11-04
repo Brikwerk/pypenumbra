f8524ef488e42287d568f4199783d0d3a7dcdd69
Brikwerk <reece@brikwerk.com>
2019-11-04 15:49:57 -0800

    Corrected path to test images

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from skimage import io, img_as_ubyte
 from skimage.exposure import equalize_adapthist
 import os
 
-focal_spot, sinogram = pypenumbra.reconstruct_from_image("./pypenumbra/test/data/penumbra_test_square.tif")
+focal_spot, sinogram = pypenumbra.reconstruct_from_image("./tests/data/penumbra_test_square.tif")
 if not os.path.isdir("./results"):
     os.makedirs("./results")
 io.imsave("./results/focal_spot.png", img_as_ubyte(equalize_adapthist(focal_spot)))
